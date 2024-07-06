567332b44604fa4edc7ce1a45fce6f2e899024ea
mkd <monkeydioude@gmail.com>
2024-07-06 16:49:50 +0200

    move config

--- src/config.rs
+++ src/config.rs
@@ -18,13 +18,13 @@ impl Settings {
         let mut s = Config::new();
 
         // Start off by merging with the "default" configuration file
-        s.merge(File::with_name("config/default"))?;
+        s.merge(File::with_name("config/patishie/default"))?;
 
         // Add in the current environment file
         // Default to 'development' env
         // Note: This will look for a file named `config/{environment}.json` or a similar format
         let env = std::env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
-        s.merge(File::with_name(&format!("config/{}", env)).required(false))?;
+        s.merge(File::with_name(&format!("config/patishie/{}", env)).required(false))?;
 
         // Add in settings from environment variables (with a prefix of APP)
         // Eg.. `APP_DEBUG=1 would set the `debug` key
